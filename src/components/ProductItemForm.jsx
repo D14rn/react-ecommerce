bae1440af8e9f722886fc72265540cbca937515c
D14rn <49399494+D14rn@users.noreply.github.com>
2024-04-08 14:19:58 +0200

    [Feat] Add product to cart button

--- src/components/ProductItemForm.jsx
+++ src/components/ProductItemForm.jsx
@@ -2,18 +2,31 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { useContext } from 'react';
+import CartItemsContext from '../contexts/CartItemsContext';
 
 
-const ProductItemForm = ({productPrice}) => {
+const ProductItemForm = ({product}) => {
+
+    const handleSubmit = (event) => {
+        const form = event.target;
+        const productAmount = form[0].valueAsNumber;
+        console.log("Produit:", product);
+        console.log("Nombre de produits à ajouter:", productAmount);
+        handleAdd(product, productAmount);
+    }
+
+    const handleAdd = useContext(CartItemsContext);
+
     return (
-        <Form className='p-3 bg-body-tertiary'>
+        <Form className='p-3 bg-body-tertiary' onSubmit={handleSubmit}>
             <Col>
                 <Form.Text className="text-muted">
-                    {productPrice}
+                    {"€" + product.price}
                 </Form.Text>
                 <Row>
                     <Col>
-                        <Form.Control type="number" placeholder="Amount" min="0" />
+                        <Form.Control type="number" placeholder="Amount" min="1" defaultValue="1" />
                     </Col>
                     <Col>
                     <Button variant="primary" type="submit">
