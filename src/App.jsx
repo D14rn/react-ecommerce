b9cb33024ada42378f9fa28d88d25f08e2b6f3c0
D14rn <49399494+D14rn@users.noreply.github.com>
2024-04-10 10:57:17 +0200

    [Refactor] Change cart items context positioning

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useReducer } from 'react';
 import Header from './Components/Header';
 import Products from './Components/Product/Products';
 import './App.css';
-import CartItemsContext from './Components/Contexts/CartItemsContext';
-import CartItemsDispatchContext from './Components/Contexts/CartItemsDispatchContext';
+import CartItemsContext from './Contexts/CartItemsContext';
+import CartItemsDispatchContext from './Contexts/CartItemsDispatchContext';
 import { cartReducer } from './Components/Cart/cartUtils';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductDetails from './Components/Product/ProductDetails';
+
+//const testDescription = "Consumes a charge to regenerate 4.166 health and 3.125 mana every 0.5 seconds over 12 seconds, for a total of 100 health and 75 mana. During this time, damaging basic attacks and ability damage against enemy champions burn them, dealing 15 bonus magic damage over 3 seconds, increased to 20 while the user cannot regenerate mana. Bonus damage is halved if applied by area of effect or damage over time."
+const testDescription = "650 Health20 Ability HasteVendettaYou gain Vendetta stacks over time Each stack of Vendetta grants you 1% reduced damage from your Nemesis.VengeanceAt maximum stacks, your Nemesis has reduced Tenacity while near you.Active -VowChoose a Nemesis to start building a Vendetta."
+const testProductName = "Anathema's Chains"
+const testProductImage = "/placeholderProductImage.png"
 
 const cartItemsTest = [
     { ref: "30e99341347c49043afec20f701", name: "Produit 1", price: 100, amount: 2 },
@@ -19,17 +25,17 @@ function App() {
 
     return (
         <>
-            <CartItemsContext.Provider value={cartState}>
                 <CartItemsDispatchContext.Provider value={cartDispatch}>
                     <BrowserRouter>
-                        <Header />
+                        <CartItemsContext.Provider value={cartState}>
+                            <Header />
+                        </CartItemsContext.Provider>
                         <Routes>
                             <Route path="/" element={<Products />} />
-                            <Route path="/products/:id" element={<p>{"ahahah"}</p>}/>
+                            <Route path="/products/:id" element={<ProductDetails productImg={testProductImage} productName={testProductName} productDescription={testDescription}/>}/>
                         </Routes>
                     </BrowserRouter>
                 </CartItemsDispatchContext.Provider>
-            </CartItemsContext.Provider>
         </>
     );
 }
