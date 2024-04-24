3dfda88aac015ce5966787673ec2605338fb8ddc
D14rn <49399494+D14rn@users.noreply.github.com>
2024-04-24 22:59:23 +0200

    [Refactor] Remove taking url into account for product fetching (because it makes the code a mess and I couldn't find a satisfying solution)

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,32 +1,25 @@
-import { useContext, useEffect, useState } from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
-import ProductItem from './subcomponents/ProductItem';
+import { useContext, useState } from 'react';
 import useFetchData from '../../CustomHooks/useFetchData';
 import Loader from '../Common/Loader';
 import Error from '../Common/Error';
-import queryString from 'query-string';
-import { useLocation, useNavigate } from 'react-router-dom';
 import ProductsPageContext from '../../Contexts/ProductsPageContext';
 import './Products.css';
 import ProductPagination from './subcomponents/ProductPagination';
 import ProductSearch from './subcomponents/ProductSearch';
 import ProductList from './subcomponents/ProductList';
 
-
-
 const createProductsUrl = (pageNum) => {
     return `http://localhost:3000/api/product/?limit=6&page=${pageNum}`;
 }
 
-const navigateToPage = (navigate, pageNum) => {
-    navigate(`/?page=${pageNum}`);
+const filterProductName = (products, productName) => {
+    const filtered = products.filter(elem => elem.name.toLowerCase().includes(productName.toLowerCase()));
+    return filtered;
 }
 
 const Products = () => {
     let products;
     let filteredProducts;
-    const location = useLocation();
-    const navigate = useNavigate();
     const [pageNum, setPageNum] = useContext(ProductsPageContext);
 
     const [url, setUrl] = useState(createProductsUrl(pageNum));
@@ -34,35 +27,11 @@ const Products = () => {
     const [categories, cateLoading, cateError] = useFetchData("http://localhost:3000/api/category");
     const [productNameFilter, setProductNameFilter] = useState("");
 
-    const filterProductName = (products, productName) => {
-        const filtered = products.filter(elem => elem.name.toLowerCase().includes(productName.toLowerCase()));
-        return filtered;
+
+    const newUrl = createProductsUrl(pageNum);
+    if (newUrl !== url) {
+        setUrl(newUrl);
     }
-
-    useEffect(() => {
-        const newUrl = createProductsUrl(pageNum);
-        if (newUrl !== url) {
-            setUrl(newUrl);
-            navigateToPage(navigate, pageNum);
-        }
-    }, [pageNum]);
-
-    useEffect(() => {
-        const parsed = queryString.parse(location.search);
-        if (Object.keys(parsed).length === 0) {
-            navigateToPage(navigate, pageNum);
-            return;
-        }
-        const parsedPage = parsed.page;
-        if (parsedPage < 1) {
-            navigateToPage(navigate, 1);
-            return;
-        }
-        const newPage = Math.max(1, parseInt(parsedPage)) || 1;
-        if (newPage !== pageNum) {
-            setPageNum(newPage);
-        }
-    }, [location.search]);
 
     if (data.products) {
         products = data.products.sort((a, b) => a.ratingsAverage < b.ratingsAverage);
